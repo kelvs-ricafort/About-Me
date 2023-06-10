046287124939f5a1845e6d5236bb1393a201a0bf
Kelvin C. Ricafort <kelvs.r@outlook.com>
2023-06-10 15:18:05 +0800

    Initial Commit

---
+++ AboutMe/AboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AboutMeApp.swift
+//  AboutMe
+//
+//  Created by Kelvin on 6/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
